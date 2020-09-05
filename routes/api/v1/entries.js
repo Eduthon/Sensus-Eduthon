76b6efb5685d496bab022cffa052febeec3244a3
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-09-05 23:00:10 +0530

    added a basic backend epress api

---
+++ routes/api/v1/entries.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const entriesApi = require("../../../controllers/api/v1/entries_api");
+
+router.post("/", entriesApi.index);
+router.get("/find", entriesApi.findByDate);
+router.post("/create-update", entriesApi.createUpdate);
+
+module.exports = router;
